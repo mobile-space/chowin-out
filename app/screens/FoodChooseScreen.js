0cf4f0406081a5ef982f2320cae6182209d0ceca
Jakhongir Khusanov <worldj95@gmail.com>
2018-04-17 19:36:02 -0700

    Like button has been added some small improvements such as backgraund color

--- app/screens/FoodChooseScreen.js
+++ app/screens/FoodChooseScreen.js
@@ -9,19 +9,18 @@ import {
   ActivityIndicator,
   Platform,
   Dimensions,
+  DeviceEventEmitter,
 } from 'react-native';
+import { Icon, Header, Button } from 'react-native-elements';
 import Carousel, { ParallaxImage } from 'react-native-snap-carousel';
-import {
-  Icon,
-  Header,
-  Button
-} from 'react-native-elements';
 import { LinearGradient } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+
 import { ENTRIES1 } from '../utils/food';
 
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
-function wp (percentage) {
+function wp(percentage) {
   const value = (percentage * viewportWidth) / 100;
   return Math.round(value);
 }
@@ -33,6 +32,9 @@ const sliderWidth = viewportWidth;
 const itemWidth = slideWidth + itemHorizontalMargin * 2;
 
 export default class FoodChooseScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
   constructor(props) {
     super(props);
 
@@ -42,14 +44,46 @@ export default class FoodChooseScreen extends React.Component {
       error: null,
       isLoading: true,
       updatedFood: null,
+      isFavorited: false,
     }
   }
+  componentWillMount() {
+    DeviceEventEmitter.addListener('setFoodUpdated', ({ updatedFood }) => {
+      this.foodUpdated({ updatedFood });
+    });
+  }
 
-  static navigationOptions = {
-    header: null,
-  };
+  foodUpdated = ({ updatedFood }) => {
+    this.setState({ updatedFood });
+    this.forceUpdate();
+  }
+
+  onFavoriteButtonPress = async () => {
+    const { isFavorited } = this.state;
+    const { onFavoriteButtonPressEmit } = this.props;
+
+    // DeviceEventEmitter.emit('setMyFoodUpdated');
+    this.setState({ isFavorited: !isFavorited });
+
+  }
+
+  renderFavoriteButton = () => {
+    const { food } = this.props;
+
+    return (
+      <TouchableOpacity onPress={this.onFavoriteButtonPress}>
+        <Ionicons
+          name={this.state.isFavorited ? "ios-heart" : "md-heart-outline"}
+          color={this.state.isFavorited ? 'red' : 'white'} size={35}
+          
+        />
+      </TouchableOpacity>
+    );
+
+  }
 
   _renderItem({ item, index }) {
+    const { updatedFood } = this.state;
     return (
       <View style={styles.slide}>
         <TouchableOpacity
@@ -59,10 +93,15 @@ export default class FoodChooseScreen extends React.Component {
           }
         >
           <Image style={styles.images} source={{ uri: item.illustration }} />
+          <View style={styles.foodInteraction}> 
           <View style={styles.foodInfo}>
             <Text style={styles.foodName} numberOfLines={2}>
               {item.title}
             </Text>
+          </View>
+          <View style={styles.buttonContainer}>
+              {this.renderFavoriteButton()}
+          </View>
           </View>
         </TouchableOpacity>
       </View>
@@ -98,7 +137,7 @@ export default class FoodChooseScreen extends React.Component {
 
   loadingView = () => {
     return (
-      <LinearGradient colors={['#ff9966', '#F2C94C']} style={styles.loadingView}>
+      <LinearGradient colors={['#536976', '#292E49']} style={styles.loadingView}>
         <View style={styles.activityIndicatorAndButtonContainer}>
           <ActivityIndicator size="large" />
           <View style={styles.getLocationbuttonContainer}>
@@ -107,6 +146,7 @@ export default class FoodChooseScreen extends React.Component {
               icon={{ name: 'my-location' }}
               title='Get Location'
               onPress={this.getCurrentLocation}
+              buttonStyle={styles.getLocationButton}
             />
           </View>
         </View>
@@ -150,16 +190,16 @@ export default class FoodChooseScreen extends React.Component {
           />
         </SafeAreaView>
         <LinearGradient colors={['#536976', '#292E49']} style={styles.mainContainer}>
-        <View style={styles.imageContainer}>
-          <Carousel
-            ref={(c) => { this._carousel = c; }}
-            data={ENTRIES1}
-            renderItem={this._renderItem.bind(this)}
-            sliderWidth={sliderWidth}
-            itemWidth={itemWidth}
-            style={styles.carouselContainer}
-          />
-        </View>
+          <View style={styles.imageContainer}>
+            <Carousel
+              ref={(c) => { this._carousel = c; }}
+              data={ENTRIES1}
+              renderItem={this._renderItem.bind(this)}
+              sliderWidth={sliderWidth}
+              itemWidth={itemWidth}
+              style={styles.carouselContainer}
+            />
+          </View>
         </LinearGradient>
 
       </View>
@@ -198,13 +238,6 @@ const styles = StyleSheet.create({
   photoPostIcon: {
     color: 'pink',
   },
-
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around'
-  },
-
   images: {
     width: "100%",
     height: '90%',
@@ -226,10 +259,29 @@ const styles = StyleSheet.create({
   getLocationbuttonContainer: {
     marginTop: 200,
   },
+  getLocationButton:{
+    backgroundColor: "#c84343",
+    width: 300,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 5
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  foodInteraction: {
+    flex: 1,
+    // flexDirection: 'row',
+    justifyContent: 'space-between',
+    // position: 'absolute'
+  },
   foodInfo: {
     // marginRight: '55%',
-    position: 'relative',
-    bottom: Platform.OS === 'ios' ? -1 : 1,
+    // position: 'relative',
+    // bottom: Platform.OS === 'ios' ? -1 : 1,
+    // alignSelf: 'center',
   },
   foodName: {
     fontSize: 20,
@@ -238,8 +290,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
 
   },
-  slide: {
-    flex: 1,
+  buttonContainer: {
+    // flexDirection: 'row',
     justifyContent: 'center',
-  }
+    alignItems: 'center',
+    width: 45,
+    height: 56,
+    paddingRight: 0,
+    position: 'absolute',
+    bottom: Platform.OS === 'ios' ? 55 : 30,
+    right: 0,
+
+  },
 });
--- app/utils/food.js
+++ app/utils/food.js
@@ -1,30 +1,36 @@
 export const ENTRIES1 = [
     {
+        id: '1',
         title: 'Pancakes',
         subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
         illustration: 'https://i.imgur.com/HZCfIxr.jpg'
     },
     {
+        id: '2',
         title: 'Earlier this morning salad',
         subtitle: 'Lorem ipsum dolor sit amet',
         illustration: 'https://i.imgur.com/BM3FqMI.jpg'
     },
     {
+        id: '3',
         title: 'White Pocket Sunset wraps',
         subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
         illustration: 'https://i.imgur.com/b9niFND.jpg'
     },
     {
+        id: '4',
         title: 'Acrocorinth, Greece',
         subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
         illustration: 'https://i.imgur.com/HZCfIxr.jpg'
     },
     {
+        id: '5',
         title: 'The lone tree',
         subtitle: 'Lorem ipsum dolor sit amet',
         illustration: 'https://i.imgur.com/JmxbY1h.jpg'
     },
     {
+        id: '6',
         title: 'Middle Earth wraps',
         subtitle: 'Lorem ipsum dolor sit amet',
         illustration: 'https://i.imgur.com/b9niFND.jpg'
