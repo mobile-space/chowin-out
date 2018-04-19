6060a26312cfb9cf95ab2747e4592720e489e1c5
Jakhongir Khusanov <worldj95@gmail.com>
2018-04-18 17:29:02 -0700

    yelp api working in screen

--- app/screens/RestaurantsListScreen.js
+++ app/screens/RestaurantsListScreen.js
@@ -5,8 +5,12 @@ import {
   View,
   SafeAreaView,
   TouchableOpacity,
+  Alert,
+  FlatList,
 } from 'react-native';
 import { Button, Input, Header } from 'react-native-elements'
+import { ENTRIES1 } from '../utils/food';
+
 
 
 export default class RestaurantsListScreen extends React.Component {
@@ -20,63 +24,111 @@ export default class RestaurantsListScreen extends React.Component {
     headerStyle: { backgroundColor: '#c84343', borderBottomWidth: 0.5, borderBottomColor: '#aaaaaa', },
   });
   constructor(props) {
-      super(props)
-      const foodName = props.navigation.state.params && props.navigation.state.params.foodName
-      console.log("Correct food name: " + foodName)
-      this.state = {
-        API_URL: 'https://api.yelp.com',
-        RES_SEARCH_URL: '/v3/businesses/search',
-        API_KEY: 'slBJejlSuGFSOUx8vRNNN2hdBtC18Gy1zEpPR6hBrw2W4FzA6PxAdkRPvlXn46vXCWZi2z2MQph46PYaVKnDKp8MdYAWQeht42ZBzSpdEUSsaZ6gS9L4XL-hbnrKWnYx',
-        // method: 'both',
-        term: foodName || null,
-        latitude: '37.785834',
-        longitude: '-122.406417',
-        location: 'san francisco, ca',
-        radius: '9000'
+    super(props)
+    const foodName = props.navigation.state.params && props.navigation.state.params.foodName
+    console.log("Correct food name: " + foodName)
+    this.state = {
+      API_URL: 'https://api.yelp.com',
+      RES_SEARCH_URL: '/v3/businesses/search',
+      API_KEY: 'slBJejlSuGFSOUx8vRNNN2hdBtC18Gy1zEpPR6hBrw2W4FzA6PxAdkRPvlXn46vXCWZi2z2MQph46PYaVKnDKp8MdYAWQeht42ZBzSpdEUSsaZ6gS9L4XL-hbnrKWnYx',
+      term: foodName || null,
+      latitude: '37.785834',
+      longitude: '-122.406417',
+      location: 'san francisco, ca',
+      radius: '9000',
+      restaurants: null,
+      isLoading: false,
+    };
+  }
+  componentDidMount() {
+    //When the component is loaded
+    this._fetchRestaurants()
+  }
+  async _fetchRestaurants() {
+    const { API_KEY, RES_SEARCH_URL, API_URL, term, location, longitude, latitude, radius, } = this.state
+
+    this.setState({ isLoading: true })
+    try {
+      let response = await fetch(`${API_URL}${RES_SEARCH_URL}?term=${term}&latitude=${latitude}&longitude=${longitude}&radius=${radius}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+        },
+      });
+      // console.log(response)
+      let responseJSON = null
+      if (response.status === 200) {
+        responseJSON = await response.json();
+        console.log(responseJSON)
+        // const { businesses, total } = responseJSON
+        // console.log("Restaurants length:", total)
+        // console.log("Restaurants", businesses)
+
+        this.setState({
+          isLoading: false,
+          restaurants: responseJSON.businesses,
+        })
+        // console.log('should be', this.state.restaurants)
+
+      } else {
+        responseJSON = await response.json();
+        const error = responseJSON.details
+        // console.log("Server request failed " + error);
+        this.setState({ errors: responseJSON.errors })
+        Alert.alert('Unable to get you restaurants', `Reason.. ${error}!`)
       }
+    } catch (error) {
+      console.log("Server is down " + error);
+      Alert.alert('Unable to get the feed. Please try again later')
     }
-    async _fetchRestaurants() {
-      const { API_KEY, RES_SEARCH_URL, API_URL, term, location, longitude, latitude, radius } = this.state
-  
-      this.setState({ isLoading: true })
-      try {
-        const response = await fetch(`${API_URL}${RES_SEARCH_URL}?term=${term}&latitude=${latitude}&longitude=${longitude}&radius=${radius}`, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-          }
-        });
-        // console.log(response)
-        const responseJSON = await response.json();
-        if (response.status === 200) {
-          console.log(responseJSON)
-          const { businesses, total } = responseJSON
-          console.log("Restaurants length:", total)
-          console.log("Restaurants", businesses)
-  
-        } else {
-          const error = responseJSON.details
-          // console.log("Server request failed " + error);
-        }
-      } catch (error) {
-        console.log("Server is down " + error);
-      }
-    }
+  }
+  _renderRestaurants(restaurant) {
+    // const { user, liked, } = this.state;
+    const { navigate } = this.props.navigation
+
+    return (
+      <View style = {styles.restaurantsRowContainer} key={restaurant}>
+        <TouchableOpacity onPress={() => navigate('Restaurant')}>
+          <View>
+            <Text style={styles.nameLabel}>{restaurant.name}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  contentView() {
+    const { isLoading, restaurants, } = this.state
+    console.log("Loading restaurants, so good", restaurants)
+    return (
+      <View style={styles.flatListContainer}>
+
+        <FlatList
+          data={restaurants}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => this._renderRestaurants(item)}
+          // onRefresh={() => this.getFeed()}
+          // refreshing={isLoading}
+        />
+      </View>
+    )
+  }
   render() {
     const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
-        <View style={{ alignSelf: 'center' }}><Text>This is RestaurantsScreen, here will be list of restaurants that offer clicked food</Text></View>
-        <TouchableOpacity onPress={() => navigate('Restaurant')}>
-          <Text style={{color: 'red'}}>Restaurant click</Text>
-        </TouchableOpacity>
-        <Button
+        {/* <TouchableOpacity onPress={() => navigate('Restaurant')}>
+
+          <Text style={{ color: 'red' }}>Restaurant click</Text>
+        </TouchableOpacity> */}
+        {this.contentView()}
+        {/* <Button
           title='SEARCH'
           buttonStyle={{
             paddingHorizontal: 5
           }}
           onPress={() => this._fetchRestaurants()}
-        />
+        /> */}
       </View>
     );
   }
@@ -91,5 +143,22 @@ const styles = StyleSheet.create({
   },
   navBar: {
     color: 'white'
-  }
+  },
+
+  flatListContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'pink',
+    alignContent: 'center',
+  },
+  restaurantsRowContainer: {
+    backgroundColor: 'white',
+  },
+  nameLabel: {
+    fontSize: 18,
+    color: '#003366',
+    marginLeft: 10,
+    fontWeight: 'bold'
+  },
+
 });
